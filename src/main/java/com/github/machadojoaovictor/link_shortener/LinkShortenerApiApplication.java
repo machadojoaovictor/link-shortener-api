35ec6a4c49b46bfe230df618421d461f6c6167ed
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2025-08-19 16:11:11 -0300

    :tada: feat: initial commit

---
+++ src/main/java/com/github/machadojoaovictor/link_shortener/LinkShortenerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.machadojoaovictor.link_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkShortenerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/machadojoaovictor/link_shortener/LinkShortenerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.machadojoaovictor.link_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
